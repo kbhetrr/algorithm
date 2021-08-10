0f108c67435f545a75aaac4d5758c7ea49bab063
KyoungHwan Kim <kimkh7534@naver.com>
2021-08-10 09:15:01 +0900

    2021.08.10

---
+++ BOJ/20254.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a, b, c, d;
+
+int main() {
+	ios_base :: sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> a >> b >> c >> d;
+	cout << (a * 56) + (b * 24) + (c * 14) + (d * 6);
+
+	return 0;
+}
+
---
+++ BOJ/8437.py
@@ -0,0 +1,5 @@
+n = int(input())
+m = int(input())
+
+print(int((n - m) // 2 + m))
+print(int((n - m) // 2))
